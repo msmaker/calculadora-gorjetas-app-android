dd4fdb1ffd2b2ab88b7171b3b4e9cdc05eef5227
Marco Silva <marcosilva.tec@gamil.com>
2021-07-06 16:20:09 -0300

    Recuperando valor digitado

--- app/src/main/java/com/msmaker/calculadoradegorjetas/MainActivity.java
+++ app/src/main/java/com/msmaker/calculadoradegorjetas/MainActivity.java
@@ -3,12 +3,62 @@ package com.msmaker.calculadoradegorjetas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editValor;
+    private TextView textProcentagem;
+    private TextView textGorjeta;
+    private TextView textTotal;
+    private SeekBar seekBarGorjeta;
+
+    private double porcentagem = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editValor = findViewById(R.id.EditValor);
+        textProcentagem = findViewById(R.id.textPorcentagem);
+        textGorjeta = findViewById(R.id.textGorjeta);
+        textTotal = findViewById(R.id.textTotal);
+        seekBarGorjeta = findViewById(R.id.seekBarGorjeta);
+
+        //Adicionar Listener SeekBar
+
+        seekBarGorjeta.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                porcentagem = progress;
+                textProcentagem.setText(Math.round(porcentagem) + "%");
+                calcular();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+
+    public void calcular() {
+        String valorRecuperado = editValor.getText().toString();
+        if (valorRecuperado == null || valorRecuperado.equals("")) {
+            Toast.makeText(getApplicationContext(),
+                    "Digite um valor primeiro",
+                    Toast.LENGTH_SHORT).show();
+        } else {
+
+        }
     }
 }
